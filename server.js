2c418f95ebe22c18c7185bfb98f4d40caba4bc58
deusexmachina892 <deusexmachina892@gmail.com>
2018-10-10 09:38:58 -0400

    Production configuration

--- server.js
+++ server.js
@@ -2,6 +2,18 @@ const express = require('express');
 
 const app = express();
 
-const port = 5000;
+//Production Setup
+if(process.env.NODE_ENV === "production"){
+    //for serving production assets
+    app.use(express.static('client/build'));
+
+    //for serving index.html, if route called is not recognized
+    const path = require('path');
+    app.get('*', (req, res)=>{
+        res.sendFile(path.resolve('client', 'build', 'index.html'));
+    })
+}
+
+const port = process.env.PORT || 5000;
 
 app.listen(port, ()=> console.log(`Server is running on port ${port}`));
