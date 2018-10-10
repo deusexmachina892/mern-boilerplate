a3a6df662907460a2f05b9b263ebe1fc42948fe3
deusexmachina892 <deusexmachina892@gmail.com>
2018-10-10 08:42:56 -0400

    Development setup

---
+++ config/dev.js
@@ -0,0 +1,5 @@
+//use this for your development keys
+module.exports = {
+    //enter development database path here
+    mongoURI: ""
+}
---
+++ config/prod.js
@@ -0,0 +1,5 @@
+//use this for your production keys
+//ideally set your production keys as enviroment variables on the server
+module.exports={
+    mongoURI = process.env.MONGO_URI
+}
